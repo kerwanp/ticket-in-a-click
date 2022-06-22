e34de2c177c33f9c42dd26345353894331ab722c
Martin PAUCOT <m.paucot@kmp.agency>
2022-06-22 21:26:41 +0200

    feat: setup chrome release

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,35 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: {
-    tioc: './src/index.tsx'
+    tioc: "./src/index.tsx",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  devtool: 'inline-source-map',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
-  plugins: [new HtmlWebpackPlugin({
-      template: './public/index.html'
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CopyPlugin({
+      patterns: ['manifest.json']
+    })
+  ],
 };
